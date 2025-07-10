b361edc23578f1c644c640c5cfdfd74a6478f0a8
Hariharan <hariharanjayachandran04@gmail.com>
2025-07-10 16:20:58 +0530

    header and footer bug fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,51 +1,16 @@
 "use client";
 import React from "react";
-import { usePathname } from "next/navigation";
-import Image from "next/image";
-import Link from "next/link";
 import styles from "./Footer.module.scss";
-import Home from "@icons/home.svg";
-import Remittance from "@icons/remittance.svg";
-import User from "@icons/footer-user.svg";
-import Inventory from "@icons/coin_black.svg";
-import Investment from "@icons/investment.svg";
+import Menu from "@icons/menu.svg";
+import Image from "next/image";
+import { MenuList } from "../common/MenuItems";
 
 const Footer = ({ hideMobileMenu = false }) => {
-  const pathname = usePathname();
-
-  const menuItems = [
-    { path: "/dashboard", text: "Home", icon: Home },
-    { path: "/remittance", text: "Remittance", icon: Remittance },
-    { path: "/profile", text: "Customer", icon: User },
-    { path: "/viewChequeLeaf", text: "Inventory", icon: Inventory },
-    { path: "/fund", text: "Investment", icon: Investment },
-  ];
-
-  const isActive = (item) => {
-    const isDashboardActive =
-      item.path === "/dashboard" &&
-      ["/viewMaturityDue", "/viewChequeLeaf"].includes(pathname);
-    const isProfileActive =
-      item.path === "/profile" &&
-      (pathname.includes("customer") || pathname === "/family-members");
-    return pathname === item.path || isDashboardActive || isProfileActive;
-  };
-
   return (
     <footer className={styles.footer}>
       <div className={styles.nav}>
         <div className={styles.desktopMenu}>
-          {menuItems.map((item, index) => (
-            <Link
-              key={index}
-              href={item.path}
-              className={isActive(item) ? styles.active : ""}
-              prefetch
-            >
-              <Image src={item.icon} priority alt="Menu Icon" />
-              <span>{isActive(item) ? item.text : ""}</span>
-            </Link>
-          ))}
+          <MenuList showLabel={false} activeClass={styles.active} />
         </div>
 
         {!hideMobileMenu && (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,18 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./Header.module.scss";
 import leftArrow from "@icons/arrow-left.svg";
 import Menu from "@icons/menu.svg";
-import Home from "@icons/home.svg";
-import Remittance from "@icons/remittance.svg";
-import User from "@icons/footer-user.svg";
-import Inventory from "@icons/coin_black.svg";
-import Investment from "@icons/investment.svg";
-import { usePathname,useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Footer from "../Footer/Footer";
 import { useTitle } from "@/contexts/TitleContext";
+import { MenuList } from "../common/MenuItems";
 
 const Header = ({ showLeftArrow, navigate = "/dashboard", footer = true }) => {
   const { title } = useTitle();
   const router = useRouter();
   const [showMenu, setShowMenu] = useState(false);
-
-  const pathname = usePathname();
-
-  const menuItems = [
-    { path: "/dashboard", text: "Home", icon: Home },
-    { path: "/remittance", text: "Remittance", icon: Remittance },
-    { path: "/profile", text: "Customer", icon: User },
-    { path: "/viewChequeLeaf", text: "Inventory", icon: Inventory },
-    { path: "/fund", text: "Investment", icon: Investment },
-  ];
-
-  const isActive = (item) => {
-    const isDashboardActive =
-      item.path === "/dashboard" &&
-      ["/viewMaturityDue", "/viewChequeLeaf"].includes(pathname);
-    const isProfileActive =
-      item.path === "/profile" &&
-      (pathname.includes("customer") || pathname === "/family-members");
-    return pathname === item.path || isDashboardActive || isProfileActive;
-  };
 
   const handleBack = () => {
     if (window.history.length > 1) {
@@ -60,29 +35,16 @@ const Header = ({ showLeftArrow, navigate = "/dashboard", footer = true }) => {
         </div>
 
         <div className={styles.footerSection}>
-          {/* Mobile menu trigger */}
           <div className={styles.mobileMenu}>
             <Image src={Menu} alt="Menu" onClick={() => setShowMenu(!showMenu)} />
           </div>
 
-          {/* Mobile popup menu */}
           {showMenu && (
             <div className={styles.popupMenu}>
-              {menuItems.map((item, index) => (
-                <Link
-                  key={index}
-                  href={item.path}
-                  className={isActive(item) ? styles.active : ""}
-                  onClick={() => setShowMenu(false)}
-                >
-                  <Image src={item.icon} priority alt="Menu Icon" />
-                  <span>{item.text}</span>
-                </Link>
-              ))}
+              <MenuList onClick={() => setShowMenu(false)} activeClass={styles.active} />
             </div>
           )}
 
-          {/* Footer for desktop */}
           {footer && <Footer hideMobileMenu />}
         </div>
       </header>
